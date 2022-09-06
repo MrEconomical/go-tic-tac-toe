a4f5de8bdd5a69bafe034a2ba08d6b79c7b3a5db
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2022-09-05 22:14:11 -0700

    add print board

--- main.go
+++ main.go
@@ -14,7 +14,27 @@ func main() {
 	}
 	//turn := 1
 
-	fmt.Println(board)
+	displayBoard(&board)
+}
+
+// Print board to terminal with row and column labels
+
+func displayBoard(board *[3][3]int) {
+	fmt.Println("  1 2 3")
+	for r, row := range board {
+		fmt.Printf("%v ", r + 1)
+		for _, s := range row {
+			if s == 0 {
+				fmt.Print("_ ")
+			} else if s == 1 {
+				fmt.Print("X ")
+			} else {
+				fmt.Print("O ")
+			}
+		}
+		fmt.Println(r + 1)
+	}
+	fmt.Println("  1 2 3")
 }
 
 // Clear terminal screen
