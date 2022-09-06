efd8b600b3dbf18ddffd346cce37cb4c3e9134d1
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2022-09-05 21:55:39 -0700

    add clear screen

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hi")
+	// Initialize board and turn counter
+	// Board square: 0 = Empty, 1 = Player X, 2 = Player O
+	// Turn: 1 = Player X, 2 = Player O
+
+	board := [3][3]int{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+	//turn := 1
+
+	fmt.Println(board)
+}
+
+// Clear terminal screen
+
+func clearScreen() {
+	fmt.Println("\033[2J\033[H")
 }
